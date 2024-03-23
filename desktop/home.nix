d1c67fa4c9cbaf3a28864bb059682fd924104a08
Caleb Bredekamp <caleb_sb@icloud.com>
2024-03-23 11:48:48 +0200

    Gen 0.11 - Discord desktop entry

--- desktop/home.nix
+++ desktop/home.nix
@@ -9,6 +9,16 @@
     WLR_NO_HARDWARE_CURSORS = 1;
   };
 
+  xdg.desktopEntries.discord = {
+    name = "Discord";
+    genericName = "All-in-one cross-platform voice and text chat for gamer";
+    exec = "discord --disable-gpu";
+    icon = "discord";
+    type = "Application";
+    categories = ["Network" "InstantMessaging"];
+    mimeType = ["x-scheme-handler/discord"];
+  };
+
   home.packages = with pkgs; [
     blender_4_0
     cura
